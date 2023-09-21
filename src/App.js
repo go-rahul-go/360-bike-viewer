dcd437d73d950e6bf837bb5d541ba6fe7834194f
rahul <rahulgoswami1520@gmail.com>
2023-09-21 18:16:48 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useRef, useState } from "react"
+import images from "./Images"
+import "./style.css"
 function App() {
+  const [pressed, setPressed] = useState(false)
+  const [index, updateIndex] = useState(0)
+
+  const [x, setX] = useState();
+  const [current,setCurrent]=useState(0)
+
+  const mouseRef = useRef();
+  const box=useRef();
+
+  useEffect(() => {
+    mouseRef.current.draggable = false;
+    box.current.draggable=false;
+    
+  }, [])
+  function mouseDown(e) {
+    setPressed(true);
+    mouseRef.current.style.cursor = "grabbing";
+    setX(e.clientX)
+
+  }
+  function mouseUp() {
+
+   
+    mouseRef.current.style.cursor = "grab";
+    setPressed(false)
+  }
+  function mouseEnter() {
+
+    mouseRef.current.style.cursor = "grab";
+  }
+  function mouseMove(e) {
+    if (!pressed)
+      return
+    else
+      {
+        
+       
+       
+        let op=e.clientX%4===0;
+        if(op &&  e.clientX<x)
+        {
+          console.log(op)
+          if(index>0)
+          {
+            updateIndex(index-1)
+          }
+          if(index===0)
+            updateIndex(15)
+        }
+        else if(op && e.clientX>x)
+        {
+          console.log(op)
+          if(index<15)
+          {
+            updateIndex(index+1)
+          }
+          if(index===15)
+            updateIndex(0)
+        }
+
+      }
+    }
+   
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="bike-box" ref={box}>
+
+
+        <img className="slide" src={images[index]} alt="" onMouseDown={(e)=>mouseDown(e)} ref={mouseRef} onMouseEnter={mouseEnter}
+          onMouseUp={mouseUp} onMouseLeave={() => setPressed(false)} onMouseMove={(e) => mouseMove(e)} title="yezdi adventure"/>
+        <p>Drag to rotate</p>
+
+
+      </div>
+    </>
   );
 }
 
---
+++ src/Images.js
@@ -0,0 +1,20 @@
+import image1 from "./images/1.png";
+import image2 from "./images/2.png";
+import image3 from "./images/3.png";
+import image4 from "./images/4.png";
+import image5 from "./images/5.png";
+import image6 from "./images/6.png";
+import image7 from "./images/7.png";
+import image8 from "./images/8.png";
+import image9 from "./images/9.png";
+import image10 from "./images/10.png";
+import image11 from "./images/11.png";
+import image12 from "./images/12.png";
+import image13 from "./images/13.png";
+import image14 from "./images/14.png";
+import image15 from "./images/15.png";
+import image16 from "./images/16.png";
+
+const images=[image1,image2,image3,image4,image5,image6,image7,image8,image9,image10,image11,image12,image13,image14,image15,image16];
+
+export default images;
